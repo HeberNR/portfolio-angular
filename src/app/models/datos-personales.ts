afde3f804410db67fa1d6686e28351de484bbfce
ramir <heber96nr@gmail.com>
2025-11-12 21:19:49 -0300

    Se corrige uso de interfaces

--- src/app/models/datos-personales.ts
+++ src/app/models/datos-personales.ts
@@ -1,2 +1,13 @@
 export interface DatosPersonales {
+    nombre: string;
+    titulo: string;
+    institucion: string;
+    descripcion: string;
+    bio: string;
+    foto: string;
+    email: string;
+    telefono: string;
+    ubicacion: string;
+    linkedin: string;
+    github: string;
 }
--- src/app/models/proyecto.ts
+++ src/app/models/proyecto.ts
@@ -1,2 +1,10 @@
 export interface Proyecto {
+    id: number;
+    titulo: string;
+    descripcion: string;
+    imagen: string;
+    tecnologias: string[];
+    linkrepositorio: string;
+    asignatura: string;
+    docente: string;
 }
